826e8b3d67729879cd1cac4b1beff6a8fa5e658a
Asad <as9vd@virginia.edu>
2023-08-17 16:12:38 -0400

    Now shows the first result when searching for an album. How good is that?

--- AndroidStuff/app/src/main/java/com/asadshamsiev/spotifyexplorationapplication/MainActivity.kt
+++ AndroidStuff/app/src/main/java/com/asadshamsiev/spotifyexplorationapplication/MainActivity.kt
@@ -8,11 +8,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -21,6 +22,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.adamratzman.spotify.SpotifyAppApi
 import com.adamratzman.spotify.endpoints.pub.SearchApi
+import com.adamratzman.spotify.models.SpotifySearchResult
 import com.adamratzman.spotify.spotifyAppApi
 import com.asadshamsiev.spotifyexplorationapplication.ui.theme.AppTheme
 import com.facebook.flipper.android.AndroidFlipperClient
@@ -31,7 +33,10 @@ import com.facebook.soloader.SoLoader
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     private val clientId = "d6d33d89d3a044618291f268d1eea409"
@@ -49,6 +54,7 @@ class MainActivity : ComponentActivity() {
             .showAuthView(true)
             .build()
 
+        // 1. Connect the Spotify connected to the local app on my phone.
         SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
             override fun onConnected(appRemote: SpotifyAppRemote) {
                 spotifyAppRemote = appRemote
@@ -60,6 +66,7 @@ class MainActivity : ComponentActivity() {
             }
         })
 
+        // 2. Connect the Spotify API that'll call the public search shit.
         lifecycleScope.launch {
             buildSpotifyPublicApi()
         }
@@ -93,11 +100,48 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private suspend fun searchForResult(query: String): SpotifySearchResult? {
+        var res: SpotifySearchResult? = null
+
+        if (::publicSpotifyAppApi.isInitialized) {
+            // Otherwise, might block the main thread.
+            res = withContext(Dispatchers.IO) {
+                publicSpotifyAppApi.search.search(
+                    query = query,
+                    searchTypes = listOf(SearchApi.SearchType.Album).toTypedArray()
+                )
+            }
+        }
+
+        return res
+    }
+
     @Composable
     @OptIn(ExperimentalMaterial3Api::class)
     fun MainScreen() {
         val textFieldQuery = remember { mutableStateOf("") }
-        val songFound = remember { mutableStateOf("No song found mate.") }
+        val foundStuff = remember { mutableListOf("") }
+        val isLoading = remember { mutableStateOf(false) }
+
+        LaunchedEffect(textFieldQuery.value) {
+            if (textFieldQuery.value.isNotEmpty()) {
+                // In order to show a loading Composable whilst results are fetched.
+                isLoading.value = true
+
+                delay(1000L) // Debounce for 1 second.
+                val result = searchForResult(textFieldQuery.value)
+                foundStuff.clear()
+
+                // Just for testing.. teehee!
+                if (result?.albums != null && result.albums?.size !!> 0) {
+                    val firstAlbum = result.albums?.get(0)?.artists?.get(0)?.name + " - " +
+                            result.albums?.get(0)?.name
+                    foundStuff.addAll(listOf(firstAlbum))
+                }
+
+                isLoading.value = false
+            }
+        }
 
         Column(
             modifier = Modifier
@@ -116,28 +160,21 @@ class MainActivity : ComponentActivity() {
                     textFieldQuery.value = it
                 }
             )
-            Button(onClick = {
-                spotifyAppRemote?.playerApi?.play("spotify:track:0VgkVdmE4gld66l8iyGjgx")
-            }) {
-                Text("Click to play Mask Off")
-            }
-            Button(onClick = {
-                if (::publicSpotifyAppApi.isInitialized) {
-                    lifecycleScope.launch {
-                        val res = publicSpotifyAppApi.search.search(
-                            query = "Playboi Carti Sky",
-                            searchTypes = listOf(SearchApi.SearchType.Track).toTypedArray()
-                        )
-                        songFound.value =
-                            (res.tracks?.get(0)?.artists.toString() +
-                                res.tracks?.get(0)?.name)
-                    }
 
+            when {
+                textFieldQuery.value.isEmpty() -> {
+                    Text("Type something you pagan") // When nothing's been typed yet.
                 }
-            }) {
-                Text("Click to do some fancy search bollocks")
+                isLoading.value -> {
+                    CircularProgressIndicator() // Show that it's visibly fetching results
+                }
+                foundStuff.isNotEmpty() -> {
+                    Text(foundStuff[0]) // Else, show the result.
+                }
+                else -> {
+                    Text("No results found.") // Terrible search.
+                }
             }
-            Text("Sound found:" + songFound.value)
         }
     }
 }
