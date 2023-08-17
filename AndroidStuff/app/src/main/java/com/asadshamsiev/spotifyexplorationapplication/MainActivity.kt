1bcebfd2a183c59272b067a7035ff2e967c4cc1f
Asad <as9vd@virginia.edu>
2023-08-17 12:09:41 -0400

    Updated styling rudimentarily and added functional TextField.

--- AndroidStuff/app/src/main/java/com/asadshamsiev/spotifyexplorationapplication/MainActivity.kt
+++ AndroidStuff/app/src/main/java/com/asadshamsiev/spotifyexplorationapplication/MainActivity.kt
@@ -3,9 +3,22 @@ package com.asadshamsiev.spotifyexplorationapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import com.asadshamsiev.spotifyexplorationapplication.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,13 +26,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                Column {
-                    Text("Start a Session")
-                    Button(onClick = {}) {
-                        Text("Click to start typing")
-                    }
-                }
+                MainScreen()
             }
+        }
+    }
+
+    @Composable
+    @OptIn(ExperimentalMaterial3Api::class)
+    fun MainScreen() {
+        val textFieldQuery = remember { mutableStateOf("") }
+        Column(
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Text("Start a Session")
+            TextField(
+                value = textFieldQuery.value,
+                placeholder = {
+                    Text("Click to start typing")
+                },
+                onValueChange = {
+                    textFieldQuery.value = it
+                }
+            )
         }
     }
 }
