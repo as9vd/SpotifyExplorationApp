e24eba4959fee3d6bec924ff86cd5561ad82a684
Asad <as9vd@virginia.edu>
2023-09-23 20:35:38 -0400

    Filled the gap between tracks and search with a lot of space, and the button is there.

--- AndroidStuff/app/src/main/java/com/asadshamsiev/spotifyexplorationapplication/TrackListSection.kt
+++ AndroidStuff/app/src/main/java/com/asadshamsiev/spotifyexplorationapplication/TrackListSection.kt
@@ -6,6 +6,7 @@ import android.os.Looper
 import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.FiniteAnimationSpec
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
@@ -50,6 +51,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.adamratzman.spotify.models.SimpleTrack
 import com.asadshamsiev.spotifyexplorationapplication.utils.SimpleTrackWrapper
 import com.asadshamsiev.spotifyexplorationapplication.utils.TrackUtils
@@ -73,36 +75,35 @@ fun TrackListSection(
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             // This button is the thing that actually starts the sampling.
             val currentIntervalIndex = viewModel.currentIntervalIndex
 
-            Crossfade(
-                targetState = isLoadingTracks, animationSpec = tween(durationMillis = 1500)
-            ) { isLoading ->
-                if (isLoading) {
-                    Text("Loading tracks..")
-                } else {
-                    ExploreAlbumButton(
-                        viewModel = viewModel,
-                        currentIntervalIndex = currentIntervalIndex
-                    )
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.height(88.dp)
+            ) {
+                Crossfade(
+                    targetState = isLoadingTracks,
+                    animationSpec = tween(durationMillis = 500, easing = FastOutSlowInEasing),
+                    label = "Loading to Button"
+                ) { isLoading ->
+                    if (!isLoading) {
+                        ExploreAlbumButton(
+                            viewModel = viewModel,
+                            currentIntervalIndex = currentIntervalIndex
+                        )
+                    } else {
+
+                    }
                 }
             }
-
 
             Spacer(modifier = Modifier.size(8.dp))
             Box(Modifier.border(BorderStroke(1.dp, Color.Black))) {
                 Column {
-                    // Need one of these at the top.
-                    Divider(
-                        color = Color.Black,
-                        modifier = Modifier
-                            .height(1.dp)
-                            .fillMaxWidth()
-                    )
-
                     for (track in uniqueTracks) {
                         val isVisible = remember { mutableStateOf(false) }
                         LaunchedEffect(track.track.id) {
@@ -403,63 +404,71 @@ fun ExploreAlbumButton(
             null
         }
 
-    Column(verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         if (!viewModel.isExploreSessionStarted) {
-            Button(
-                elevation = ButtonDefaults.elevatedButtonElevation(),
-                border = BorderStroke(1.dp, Color.Black),
-                onClick = onClick
-            ) {
-                Text("Start Exploring")
-            }
+
+                Button(
+                    elevation = ButtonDefaults.elevatedButtonElevation(),
+                    border = BorderStroke(1.dp, Color.Black),
+                    onClick = onClick
+                ) {
+                    Text("Start Exploring")
+                }
+
         } else {
-
-            Button(
-                elevation = ButtonDefaults.elevatedButtonElevation(),
-                border = BorderStroke(1.dp, Color.Black),
-                onClick = onClick
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("Stop Exploring")
-            }
+                Button(
+                    elevation = ButtonDefaults.elevatedButtonElevation(),
+                    border = BorderStroke(1.dp, Color.Black),
+                    onClick = onClick
+                ) {
+                    Text("Stop Exploring")
+                }
 
-            Spacer(modifier = Modifier.size(8.dp))
+                Spacer(modifier = Modifier.size(8.dp))
 
-            // Give it a little crossfade so it doesn't abruptly enter/leave.
-            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                var i = trackStartIndices.value[currentTrack?.track?.id]
-                val amountOfIntervals = currentAlbumTracks.size
-                if (i != null) {
-                    // Iterate until the next 3 tracks pretty much.
-                    while (i < amountOfIntervals && currentAlbumTracks[i].first == currentTrack) {
-                        val interval = currentAlbumTracks[i].second
+                // Give it a little crossfade so it doesn't abruptly enter/leave.
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    var i = trackStartIndices.value[currentTrack?.track?.id]
+                    val amountOfIntervals = currentAlbumTracks.size
+                    if (i != null) {
+                        // Iterate until the next 3 tracks pretty much.
+                        while (i < amountOfIntervals && currentAlbumTracks[i].first == currentTrack) {
+                            val interval = currentAlbumTracks[i].second
 
-                        // For each interval we've got for the song (3 for > 45 seconds), create a duration card for it.
-                        val duration = "${interval.first} - ${interval.second}"
+                            // For each interval we've got for the song (3 for > 45 seconds), create a duration card for it.
+                            val duration = "${interval.first} - ${interval.second}"
 
-                        Crossfade(
-                            targetState = (i == currentIntervalIndex.value),
-                            label = "Transition Active Interval"
-                        ) { isSelected ->
-                            val containerColor =
-                                if (isSelected) MaterialTheme.colorScheme.onPrimaryContainer else MaterialTheme.colorScheme.onSurface
+                            Crossfade(
+                                targetState = (i == currentIntervalIndex.value),
+                                label = "Transition Active Interval"
+                            ) { isSelected ->
+                                val containerColor =
+                                    if (isSelected) MaterialTheme.colorScheme.onPrimaryContainer else MaterialTheme.colorScheme.onSurface
 
-                            Card(
-                                border = BorderStroke(1.dp, Color.Black),
-                                colors = CardDefaults.cardColors(
-                                    containerColor = containerColor
-                                ),
-                                shape = RoundedCornerShape(0),
-                            ) {
-                                Text(
-                                    duration,
-                                    modifier = Modifier.padding(4.dp),
-                                    fontWeight = FontWeight(400)
-                                )
+                                Card(
+                                    border = BorderStroke(1.dp, Color.Black),
+                                    colors = CardDefaults.cardColors(
+                                        containerColor = containerColor
+                                    ),
+                                    shape = RoundedCornerShape(0),
+                                ) {
+                                    Text(
+                                        duration,
+                                        modifier = Modifier.padding(4.dp),
+                                        fontWeight = FontWeight(400)
+                                    )
+                                }
                             }
-                        }
 
-                        i++
+                            i++
+                        }
                     }
                 }
             }
